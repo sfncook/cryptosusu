3a20d63cf0d90cc4af20f2c56c822060323fc261
Shawn Cook <cooksha@yahoo-inc.com>
2018-07-23 03:48:03 -0700

    DeployPage items

--- src/components/DeployPage.js
+++ src/components/DeployPage.js
@@ -9,12 +9,40 @@ class DeployPage extends Component {
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1" style={{paddingTop:'15px'}}>
-            <h1>Deploy Page</h1>
+            <table className="groupTable">
+              <tbody>
+              <tr id="memberTemplate">
+                <th>Group Name:</th>
+                <td><input/></td>
+              </tr>
+              <tr id="memberTemplate">
+                <th>Many Partners:</th>
+                <td><input/></td>
+              </tr>
+              <tr id="memberTemplate">
+                <th>Payout Frequency:</th>
+                <td><input/></td>
+              </tr>
+              <tr id="memberTemplate">
+                <th>Contribution Amt (eth):</th>
+                <td><input/></td>
+              </tr>
+              </tbody>
+            </table>
+
+            <div className="pure-u-1-1">
+              <button onClick={(e)=>{this.clickCreate(e)}} className="btn-contribute" type="button" data-id="0">Create New Susu Group</button>
+            </div>
           </div>
         </div>
       </main>
     );
   }// render()
+
+  clickCreate(e) {
+    e.preventDefault();
+    console.log('create clicked');
+  }
 }
 
 DeployPage.defaultProps = {
